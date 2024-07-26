371eafae5c8581c6e49896f1cef89213905e214c
rchrd <26688060+rchrd-0@users.noreply.github.com>
2024-07-26 18:28:09 +0800

    new qk60 split for modtap caps

--- keyboards/qwertykeys/qk60/keymaps/rchrd-split-mod/keymap.c
+++ keyboards/qwertykeys/qk60/keymaps/rchrd-split-mod/keymap.c
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum custom_keycodes {
     RHYPR = SAFE_RANGE,
+    RMEH,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -36,6 +37,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_RGUI);
             }
             return false;
+        case RMEH:
+            if (record->event.pressed) {
+                register_code(KC_RCTL);
+                register_code(KC_RSFT);
+                register_code(KC_RALT);
+            } else {
+                unregister_code(KC_RCTL);
+                unregister_code(KC_RSFT);
+                unregister_code(KC_RALT);
+            }
+            return false;
     }
     return true;
 }
@@ -45,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 		KC_ESC,  KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-		KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
+		MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1),
-		KC_LCTL, KC_LALT, KC_LGUI, MO(1),            KC_SPC,  KC_SPC,  KC_SPC,           KC_RGUI,KC_RGUI, KC_RALT, KC_RCTL
+		KC_LCTL, KC_LALT, KC_LGUI, MO(1),            KC_BSPC,  KC_SPC,  KC_SPC,           RMEH,KC_RGUI, KC_RALT, KC_RCTL
 	),
 	[1] = LAYOUT(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DELETE,
